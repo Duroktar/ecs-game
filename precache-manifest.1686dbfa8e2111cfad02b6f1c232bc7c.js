3df435fbdf28e5a7f0d2f1e4293610c6838eb2d6
traBpUkciP <scott.doucet@hotmail.com>
2021-10-15 23:26:34 -0300

    Updates

---
+++ precache-manifest.1686dbfa8e2111cfad02b6f1c232bc7c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "efafac38d4d13d591cde",
+    "url": "/ecs-game/static/css/main.f818235e.chunk.css"
+  },
+  {
+    "revision": "efafac38d4d13d591cde",
+    "url": "/ecs-game/static/js/main.efafac38.chunk.js"
+  },
+  {
+    "revision": "9f1e0393392d808156bc",
+    "url": "/ecs-game/static/js/1.9f1e0393.chunk.js"
+  },
+  {
+    "revision": "46de768981e563071400",
+    "url": "/ecs-game/static/js/runtime~main.46de7689.js"
+  },
+  {
+    "revision": "7c857ee1d9fbc4df261e43b34c6ad795",
+    "url": "/ecs-game/static/media/sfx_wpn_laser1.7c857ee1.wav"
+  },
+  {
+    "revision": "e65a7e8bc025ea83efff538398dfc545",
+    "url": "/ecs-game/static/media/sfx_exp_long1.e65a7e8b.wav"
+  },
+  {
+    "revision": "277a07d7f8eb65c538418489ff7734b7",
+    "url": "/ecs-game/static/media/game-menu.277a07d7.mp3"
+  },
+  {
+    "revision": "62197df4a9f0a14f15650423a8075fc5",
+    "url": "/ecs-game/static/media/game-main.62197df4.mp3"
+  },
+  {
+    "revision": "4f698af102ac4bf593bb76bba96f803e",
+    "url": "/ecs-game/static/media/game-end.4f698af1.mp3"
+  },
+  {
+    "revision": "430d3f4f473d3ac68146d120bc40cf6a",
+    "url": "/ecs-game/static/media/game-main-summary.430d3f4f.mp3"
+  },
+  {
+    "revision": "aae78736e3268357be169b3628b0585c",
+    "url": "/ecs-game/static/media/sfx_sounds_fanfare1.aae78736.wav"
+  },
+  {
+    "revision": "da47f0348ec93aa9d0b114d0e4a93dd4",
+    "url": "/ecs-game/static/media/sfx_sounds_powerup3.da47f034.wav"
+  },
+  {
+    "revision": "25f989ffbe6b7d0dc4bb38ed62beaae6",
+    "url": "/ecs-game/static/media/sfx_sounds_fanfare3.25f989ff.wav"
+  },
+  {
+    "revision": "35e9b099dd53b42751ba1e3dc8eff704",
+    "url": "/ecs-game/static/media/sfx_sounds_powerup2.35e9b099.wav"
+  },
+  {
+    "revision": "6098824846a8ca06881b849d794848ea",
+    "url": "/ecs-game/static/media/sfx_sounds_interaction1.60988248.wav"
+  },
+  {
+    "revision": "b7b7a7b972dea05bf7d93b306e18e1d6",
+    "url": "/ecs-game/static/media/sfx_sounds_pause1_in.b7b7a7b9.wav"
+  },
+  {
+    "revision": "959daf2a992741ca457fd75a25d72ac0",
+    "url": "/ecs-game/static/media/sfx_sounds_pause1_out.959daf2a.wav"
+  },
+  {
+    "revision": "75bcd782ff2323454c8b0bafaa4b3e2d",
+    "url": "/ecs-game/static/media/sfx_wpn_cannon1.75bcd782.wav"
+  },
+  {
+    "revision": "c6e20b3127505ea1abf1397fa24b9a6f",
+    "url": "/ecs-game/static/media/sfx_exp_short_soft5.c6e20b31.wav"
+  },
+  {
+    "revision": "ffa98eb5ef4cf140772370ba8033c4d6",
+    "url": "/ecs-game/static/media/sfx_exp_short_hard12.ffa98eb5.wav"
+  },
+  {
+    "revision": "7a7460e66ebb7b91c8f602e7690e8095",
+    "url": "/ecs-game/static/media/sfx_exp_cluster1.7a7460e6.wav"
+  },
+  {
+    "revision": "6d3db53412cff67e25ea3806d07ea874",
+    "url": "/ecs-game/static/media/sfx_exp_odd1.6d3db534.wav"
+  },
+  {
+    "revision": "8745c3912cb29cbd6e1d7d2634e6e61c",
+    "url": "/ecs-game/static/media/ship.8745c391.png"
+  },
+  {
+    "revision": "053dad25ec55f50c1a3118b5e2608c5a",
+    "url": "/ecs-game/static/media/Galaga-Logo.053dad25.png"
+  },
+  {
+    "revision": "184328c9f690beb43765fb22997a9cca",
+    "url": "/ecs-game/static/media/GameOver.184328c9.png"
+  },
+  {
+    "revision": "62a7d47035c4de11abb6f7f524425c1c",
+    "url": "/ecs-game/index.html"
+  }
+];
